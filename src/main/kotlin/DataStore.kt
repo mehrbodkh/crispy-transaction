2e6169fc4c5a3c1670d2c509102d47a4068117c9
Mehrbod <mehrbod@ridedott.com>
2023-03-15 07:45:39 +0100

    feat: add a level of abstraction over storage

---
+++ src/main/kotlin/DataStore.kt
@@ -0,0 +1,24 @@
+
+interface KeyValueStore<K, V> {
+    fun put(key: K, value: V)
+    operator fun get(key: K): V?
+    fun remove(key: K): V?
+    fun count(predicate: (V) -> Boolean)
+    fun copy(): KeyValueStore<K, V>
+}
+
+class DataStore<K, V>(private val map: MutableMap<K, V> = mutableMapOf()) : KeyValueStore<K, V> {
+    override fun put(key: K, value: V) {
+        map[key] = value
+    }
+
+    override fun get(key: K): V? = map[key]
+
+    override fun remove(key: K) = map.remove(key)
+
+    override fun count(predicate: (V) -> Boolean) {
+        map.count { predicate(it.value) }
+    }
+
+    override fun copy(): KeyValueStore<K, V> = DataStore(map.toMutableMap())
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
 fun main() {
-    transaction(mapOf())
+    transaction(DataStore())
 }
 
-fun transaction(map: Map<String, String>): Map<String, String>? {
-    var internalMap = map.toMutableMap()
+fun transaction(map: KeyValueStore<String, String>): KeyValueStore<String, String>? {
+    var internalMap = map.copy()
     while (true) {
         print("> ")
         val (commandInput, key, value) = readCommand()
 
         when (commandInput.toCommand()) {
-            Command.SET -> internalMap[key] = value
+            Command.SET -> internalMap.put(key, value)
             Command.GET -> println(internalMap[key] ?: "key not set")
             Command.DELETE -> internalMap.remove(key)
-            Command.COUNT -> println(internalMap.count { it.value == value })
-            Command.BEGIN -> transaction(internalMap)?.let { internalMap = it.toMutableMap() }
+            Command.COUNT -> println(internalMap.count { it == value })
+            Command.BEGIN -> transaction(internalMap)?.let { internalMap = it.copy() }
             Command.ROLLBACK -> return null
             Command.COMMIT -> return internalMap
             Command.UNKNOWN -> println("incorrect input")
